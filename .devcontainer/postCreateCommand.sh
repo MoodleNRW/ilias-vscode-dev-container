53270cbf7298ce21f3d41e40c6618ed70087fb4d
RUB-bschkuhl <135697714+RUB-bschkuhl@users.noreply.github.com>
2023-07-11 11:07:51 +0000

    Anpassungen postcreate

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,6 +7,7 @@ done
 PWD="$(pwd)"
 
 # ILIAS
+
 echo -e "\nInstall ILIAS\n"
 sudo chmod 755 $PWD &&
 sudo rm -rf /var/www/html && 
@@ -29,6 +30,10 @@ if [ ! -d "$ILIASDIR" ]; then
   git clone -b release_${ILIAS_VERSION} https://github.com/ILIAS-eLearning/ILIAS.git $ILIASDIR --depth 1
 fi
 
+MIN_CONFIG_JSON='{"common": {"client_id": "iliastest","server_timezone": "Europe/Berlin"},"database": {"type": "innodb","host": "db","port": 3306,"database": "ilias_'$ILIAS_VERSION'","user": "ilias_'$ILIAS_VERSION'","password": "ilias"},"filesystem": {"data_dir": "'$DATADIR'"},"http": {"path": "'$ILIASDIR'"},"systemfolder": {"contact": {"firstname": "Admin","lastname": "Admin","email": "admin@idev.dev"}},"language": {"default_language": "en","install_languages": ["de", "en"],"install_local_languages": ["de"]}}'
+sudo echo ${MIN_CONFIG_JSON} > .devcontainer/minimal-config.json
+sudo cp .devcontainer/minimal-config.json /var/www/minimal-config.json
+
 composer -d /var/www/html/ilias-${ILIAS_VERSION} update
 
 ILIAS_VERSION_DB=${DB_NAME}_${ILIAS_VERSION}
@@ -42,6 +47,8 @@ sudo mysql -h ${DB_HOST} -u root -p${DB_ROOT_PWD} -e "CREATE USER ${ILIAS_VERSIO
 sudo mysql -h ${DB_HOST} -u root -p${DB_ROOT_PWD} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,LOCK TABLES,ALTER ON ${ILIAS_VERSION_DB}.* TO ${ILIAS_VERSION_DB_USER}@'%';"
 sudo mysql -h ${DB_HOST} -u root -p${DB_ROOT_PWD} -e "FLUSH PRIVILEGES;"
 
+
+
 sudo php /var/www/html/ilias-${ILIAS_VERSION}/setup/setup.php install /var/www/minimal-config.json --yes
 
 git config --global --add safe.directory $ILIASDIR
@@ -50,9 +57,11 @@ git config --global --add safe.directory $ILIASDIR
 (sudo crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/php /var/www/html/ilias-${ILIAS_VERSION}/cron/cron.php > /dev/null") | sudo crontab -
 done
 
-sudo a2enmod rewrite
+# sudo a2enmod rewrite
+sudo cp .devcontainer/php.ini /usr/local/etc/php/php.ini
 
-cp php.ini /usr/local/etc/php/php.ini
-chown -R www-data:www-data /var/www/iliasdata-8/iliastest
-chmod -R 775 /var/www/iliasdata-8/iliastest
-sudo /etc/init.d/apache2 reload
+if [ ! -d "$DATADIR/iliastest" ]; then
+  sudo mkdir ${DATADIR}/iliastest
+  sudo chown -R www-data:www-data ${DATADIR}/iliastest
+  sudo chmod -R 775 ${DATADIR}/iliastest
+fi
